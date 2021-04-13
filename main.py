8ded4159f8cf467e219b59e43fa4aacd496023e4
Marti March <mmll1199@protonmail.com>
2021-04-14 01:18:05 +0200

    Primer Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Aalgoritmo Harris & Stephens
+
+"""
+ Vamos a buscar aquellas zonas con una variacion de imagenes mut grande. De esta forma obtendremos los puntos de interes,
+ es decir, obtendremos aquellos puntos que forman el borde de una figura.
+"""
