5d04c0ad1c5a12805c0430309b651916ab2c2550
Marti March <mmll1199@protonmail.com>
2021-04-15 05:05:47 +0200

    Ahora es posible detectar el movimiento contando las esquinas y actualizando el número de esquinas usadas como referencia cada 15 minutos. Tambien se añaden unos umbrales sobre que se palican sobre el numero de puntos actualizados y que permiten aceptar o denegar la existencia de movimiento.

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 # Aalgoritmo Harris & Stephens
 
 """
- Vamos a buscar aquellas zonas con una variación de imágenes muy grande. De esta forma, obtendremos los puntos de interes.
- La idea del algoritmo es obtener aquellos puntos que hacen “esquina” mirando si tienen dos gradientes muy grandes en perpendicular.
+ Vamos a buscar aquellas zonas con una variacion de imagenes mut grande. De esta forma obtendremos los puntos de interes.
+ La idea del algoritmo es obtener aquellos puntos que hacen “esquina” mirando si tiene dos gradientes muy grandes en perpendicular.
 """
 
 import cv2
 import numpy as np
+from datetime import datetime
 
-camara = cv2.VideoCapture(1)
+camara = cv2.VideoCapture(2)
+actualTime = 0
+puntosRelevantes = 0
+umbralSuperior = 1.5
+umbralInferior = 0.5
+now = 0
+tiempoActual = 0
 
 while True:
     _, imagen = camara.read()
 
-    # La imagnen a color se transforma en una imagen en gris
+    # La imágnen a color se trnsforma en una imágen en gris
     imagenGris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 
-    # Se transforma la imagen en gris en un float
+    # Se transforma la imagen en gri en un float
     imagenGris = np.float32(imagenGris)
 
     """
@@ -26,20 +33,36 @@ while True:
         
         2- Tamaño de los vecinos considerados para la detección, es decir, se utiliza el rango de valores -2 < x < 2.
         
-        3- Parámetro de apertura de la derivada Sobel, es decir, M es una matriz 3x3. Cuanto más grande se la matriz más 
-           falsos positivos. Cuanto más pequeña más puntos de interés se pierden.
+        3- Parámetro de apertura de la derivada Sobel, es decir, M e una matriz 3x3. Cuanto más grande más falsos positivos. 
+           Cuanto más pequeño más puntos de interés se pierden.
            
         4- Parámetro libre en la formula de Harris. Es una constante que varía entre [0.04, 0.06].
         
-                                                                                   | -1  0  1 |  | -1 -2 -1 |
-        Las matrices que se aplicarán para buscar las esquinas son las siguientes: | -2  0  2 |  |  0  0  0 |
-                                                                                   | -1  0  1 |  |  1  2  1 |
-    
+                                                                                    -1  0  1    -1 -2 -1
+        Las matrizes que se aplicarán para buscar las esquinas son las siguientes:  -2  0  2     0  0  0
+                                                                                    -1  0  1     1  2  1
     """
     harris = cv2.cornerHarris(imagenGris, 2, 3, 0.04)
 
-    # Del resultado anterior me quedo con el 1% de las esquinas totales más relevantes. Se representrán mediante puntos en azul.
-    imagen[harris>0.01 * harris.max()] = [255, 0, 0]
+    #Obtenemos el numero total se esquinas
+    nEsquinas = np.sum(harris > 0.01 * harris.max())
 
-    cv2.imshow("Harris", imagen);
-    cv2.waitKey(0)
+    #Cada 15 minutos se renuevan los puntos de referencia usados para comparar
+    now = datetime.now()
+    if now.timestamp() - tiempoActual > 9000000 or tiempoActual == 0:
+        tiempoActual = now.timestamp()
+        puntosRelevantes = nEsquinas
+
+    # Para saber si se ha producido movimient lo que se hace es comparar si la cantidad de puntos entra dentro de un umbral.
+    print("Numero de esquinas: " + str(nEsquinas))
+    print("Umbral superior: " + str(puntosRelevantes * umbralSuperior))
+    print("Umbral inferior: " + str(puntosRelevantes * umbralInferior))
+    if nEsquinas > (puntosRelevantes * umbralSuperior) or nEsquinas < (puntosRelevantes * umbralInferior):
+        print("MOVIMIENTO")
+
+    # Del resultado anterior me quedo con el 1% de las esquinas totales más relevantes. Se represenán mediante puntos en azul.
+    imagen[harris>0.01 * harris.max()] = [0, 0, 255]
+    cv2.imshow("Harris - color", imagen);
+
+    if cv2.waitKey(2) & 0xFF == ord('q'):
+        break
